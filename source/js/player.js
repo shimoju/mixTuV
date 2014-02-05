c6124630527694f991ae710f2d7e454f69154f2d
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-02-05 22:15:49 +0900

    Add playerVars

--- source/js/player.js
+++ source/js/player.js
@@ -4,6 +4,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
+    playerVars: {'rel': 0},
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
