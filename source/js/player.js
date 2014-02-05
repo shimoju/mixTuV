850e31ab9e2046ba0b115f6648c307fb65bd52bf
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-02-05 16:11:36 +0900

    Play video using YouTube API

---
+++ source/js/player.js
@@ -0,0 +1,20 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'PqJNc9KVIZE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+}
