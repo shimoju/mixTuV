369a4491fa9c87621382ec4ba206d3ad6154ec95
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-02-06 22:07:30 +0900

    Separate utility.js

--- source/js/player.js
+++ source/js/player.js
@@ -89,16 +89,3 @@ var parseVideo = function(xml) {
     url: $(content).attr('url'),
   };
 };
-
-var shuffle = function(array) {
-  // Fisher–Yates Shuffle
-  // http://bost.ocks.org/mike/shuffle/
-  var m = array.length, t, i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-  return array;
-};
---
+++ source/js/utility.js
@@ -0,0 +1,14 @@
+// Utility
+
+var shuffle = function(array) {
+  // Fisher–Yates Shuffle
+  // http://bost.ocks.org/mike/shuffle/
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
